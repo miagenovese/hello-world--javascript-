f5f1dd6b3cc46183ff86867687ba9c94bc91174e
Mia <miagenovese@gmail.com>
2018-06-07 02:25:10 +0200

    Iterate with forEach

---
+++ foreach.js
@@ -0,0 +1,10 @@
+let favFruits = ["kiwi", "pear", "peach", "blueberry", 32];
+favFruits.forEach(function(element) {
+  console.log(element);
+});
+
+
+let soliloquy = "To be, or not to be, that is the question:";
+Array.from(soliloquy).forEach(function(character) {
+  console.log(character);
+});
